5a5a944c2f17939bcb746325853e26fb5859688f
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-06 16:04:24 +0330

    picture added to QA componet

--- sadrun-project/src/components/Q&A/QA.jsx
+++ sadrun-project/src/components/Q&A/QA.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import "../../sass/fonts.scss";
 import styles from "./QA.module.scss";
+import sectionPicture from "./images/CompositeLayer-mdpi.svg";
 
 export default function QA() {
-  return <section className={styles.QA}>
-    <div className={styles.sectionName}>پیشنهاد و انتقادات</div>
-  </section>;
+  return (
+    <section className={styles.QA}>
+      <div className={styles.sectionName}>پیشنهاد و انتقادات</div>
+      <div className={styles.mainQA}>
+        <img className={styles.QAPic} src={sectionPicture} />
+      </div>
+    </section>
+  );
 }
