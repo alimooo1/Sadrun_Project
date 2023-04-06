fd58b4fcc318411c3e52aaba3385b1f436d36c9e
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-06 17:12:41 +0330

    formQA completed

--- sadrun-project/src/components/Q&A/Form-Q&A/FormQA.jsx
+++ sadrun-project/src/components/Q&A/Form-Q&A/FormQA.jsx
@@ -4,7 +4,9 @@ import styles from "./FormQA.module.scss"
 function FormQA() {
 	return (
 		<div className={styles.formQA}>
-
+			<input className={styles.inputQA} type="text" placeholder='نام کامل' />
+			<input className={styles.inputQA} type="email" placeholder='ایمیل' />
+			<textarea className={styles.textareaQA} placeholder='متن پیام' />
 		</div>
 	)
 }
