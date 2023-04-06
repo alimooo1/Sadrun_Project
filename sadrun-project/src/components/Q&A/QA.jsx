bd2a68bf6cef7354b854047fa63cf7a7aea40edc
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-06 14:54:32 +0330

    sectionName added

--- sadrun-project/src/components/Q&A/QA.jsx
+++ sadrun-project/src/components/Q&A/QA.jsx
@@ -3,6 +3,6 @@ import styles from "./QA.module.scss";
 
 export default function QA() {
   return <section className={styles.QA}>
-
+    <div className={styles.sectionName}>پیشنهاد و انتقادات</div>
   </section>;
 }
