78598c1674622cda1b7181920cbb5428a38e1a7c
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-06 15:02:52 +0430

    articleCard add

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import CreateBtnComponent from "./components/Creat Btn Component/CreateBtnComponent";
+import CreateArticleCard from "./components/Creat article card/CreateArticleCard";
 
 function App() {
   return <div className="App">
-    <CreateBtnComponent/>
+    <CreateArticleCard/>
   </div>;
 }
 
