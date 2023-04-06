ba743ff4631bafd5c4e1d0f85e3603f5877c6617
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-06 16:45:49 +0330

    formQA layout created

---
+++ sadrun-project/src/components/Q&A/Form-Q&A/FormQA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./FormQA.module.scss"
+
+function FormQA() {
+	return (
+		<div className={styles.formQA}>
+
+		</div>
+	)
+}
+
+export default FormQA;
--- sadrun-project/src/components/Q&A/QA.jsx
+++ sadrun-project/src/components/Q&A/QA.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../sass/fonts.scss";
 import styles from "./QA.module.scss";
+import FormQA from "./Form-Q&A/FormQA";
 import sectionPicture from "./images/CompositeLayer-mdpi.svg";
 
 export default function QA() {
@@ -9,6 +10,7 @@ export default function QA() {
       <div className={styles.sectionName}>پیشنهاد و انتقادات</div>
       <div className={styles.mainQA}>
         <img className={styles.QAPic} src={sectionPicture} />
+        <FormQA />
       </div>
     </section>
   );
