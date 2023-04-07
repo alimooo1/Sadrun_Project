1d8081e74df7c06b9ab071560b15f22ede4a0746
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-07 22:38:20 +0430

    upadate style and add image in the btn component

--- sadrun-project/src/components/Creat Btn Component/CreateBtnComponent.jsx
+++ sadrun-project/src/components/Creat Btn Component/CreateBtnComponent.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { FaShoppingCart,FaSearch } from "react-icons/fa";
 import styles from "../Creat Btn Component/CreateBtnComponent.module.css"
+import shoppinIcon from "./image/Path 619.svg";
+import searchIcon from "./image/Path 761.svg";
+
 function CreateBtnComponent() {
   return (
     <div className={styles.conteiner} >
-      <button className={styles.search}><FaSearch/></button>
+      <button className={styles.shopping_cart}>
+        <img className={styles.shoppinIcon} src={shoppinIcon} alt="" />
+      </button>
       <button className={styles.register}>ثبت نام</button>
       <button className={styles.login}>ورود</button>
-      <button className={styles.shopping_cart}><FaShoppingCart/></button>
+      <button className={styles.search}>
+        <img className={styles.searchIcon} src={searchIcon} alt="" />
+        </button>
       
 
     </div>
