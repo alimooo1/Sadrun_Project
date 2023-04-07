7d4c4d6c3c2fef1530fc9718a4e1a621b88e5246
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-07 17:28:14 +0330

    Router created

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Footer from "./components/Footer/Footer";
-import QA from "./components/Q&A/QA";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
