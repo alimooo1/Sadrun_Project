dcd676cf0f0b241d734e288cdadf1d30854f8cd7
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-07 19:01:30 +0430

    upadte style component teacher card

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
-import CreatTeacherCard from "./components/CreatTeacherCard";
+import "./sass/_fonts.scss"
+import CreatTeacherCard from "./components/createTeacheCard/CreatTeacherCard";
 
 function App() {
   return <div className="App">
+   
     <CreatTeacherCard/>
   </div>;
 }
