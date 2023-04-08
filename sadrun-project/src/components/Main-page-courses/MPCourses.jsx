11f4edebb6f9b6e1285d89b94e36e76f909a3161
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-04-08 07:31:44 +0330

    more content added.

--- sadrun-project/src/components/Main-page-courses/MPCourses.jsx
+++ sadrun-project/src/components/Main-page-courses/MPCourses.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "../../sass/fonts.scss";
 import styles from "./style.module.scss";
-import graph from './assets/graph.svg';
+import graph from "./assets/graph.svg";
+import nodePic from "./assets/node.png";
+import vsCodePic from "./assets/vs-code.png";
+import angularPic from "./assets/angular.png";
+import nestPic from "./assets/nest.png";
 
 export default function MPCourses() {
   return (
@@ -11,7 +15,32 @@ export default function MPCourses() {
         <h2>دوره ها</h2>
         <span>آخرین دوره های موجود</span>
       </div>
-      <div className={styles.row}></div>
+      <div className={styles.row}>
+        <div className={styles.bigcard}>
+          <img src={nodePic} alt="" />
+
+          <div className={styles.info}>
+            <div>
+              <h3>نود جی اس : دوره کامل </h3>
+              <p>
+                نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ
+                سمت سرورِ متن‌باز و چندسکویی برای زبان جاوااسکریپت است
+              </p>
+            </div>
+            <div>
+              <span>آرمین اسلامی</span>
+              <button>مشاهده دوره</button>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.smallcard}>
+          <img src={vsCodePic} alt="" />
+          <img src={angularPic} alt="" />
+          <img src={nestPic} alt="" />
+          <button className={styles.button}>مشاهده دوره</button>
+        </div>
+      </div>
     </div>
   );
 }
