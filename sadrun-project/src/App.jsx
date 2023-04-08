711ba21b9eb0789a84941350394b597caf7329ff
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-08 18:16:39 +0430

    update style btn

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CreateBtnComponent from "./components/Creat Btn Component/CreateBtnComponent";
+import "./sass/_fonts.scss"
 
 function App() {
   return <div className="App">
--- sadrun-project/src/components/Creat Btn Component/CreateBtnComponent.jsx
+++ sadrun-project/src/components/Creat Btn Component/CreateBtnComponent.jsx
@@ -6,14 +6,15 @@ import searchIcon from "./image/Path 761.svg";
 function CreateBtnComponent() {
   return (
     <div className={styles.conteiner} >
-      <button className={styles.shopping_cart}>
-        <img className={styles.shoppinIcon} src={shoppinIcon} alt="" />
-      </button>
-      <button className={styles.register}>ثبت نام</button>
-      <button className={styles.login}>ورود</button>
+      
       <button className={styles.search}>
         <img className={styles.searchIcon} src={searchIcon} alt="" />
         </button>
+      <button className={styles.login}>ورود</button>
+      <button className={styles.register}>ثبت نام</button>
+      <button className={styles.shopping_cart}>
+        <img className={styles.shoppinIcon} src={shoppinIcon} alt="" />
+      </button>
       
 
     </div>
