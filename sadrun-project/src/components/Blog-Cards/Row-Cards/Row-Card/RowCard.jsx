5ba15647ea59464bc8db04751bfdfe48fe25b6a4
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-09 20:05:48 +0330

    Row Cards srtucture created

---
+++ sadrun-project/src/components/Blog-Cards/Row-Cards/Row-Card/RowCard.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./RowCard.module.scss";
+
+export default function RowCard() {
+  return <div></div>;
+}
---
+++ sadrun-project/src/components/Blog-Cards/Row-Cards/RowCards.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./RowCards.module.scss";
+
+export default function RowCards() {
+  return <div></div>;
+}
