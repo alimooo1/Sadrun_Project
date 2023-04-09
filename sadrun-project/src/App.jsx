396b34ec120755a65ae2610a98ab89cdea6194dd
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-09 10:45:34 +0330

    Boxy Card implemented in App.jsx for test

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import QA from "./components/Q&A/QA";
+import BoxyCard from "./components/Blog-Cards/Boxy-Cards/Boxy-Card/BoxyCard";
+import Picture from "./components/Blog-Cards/Boxy-Cards/images/nest.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BoxyCard
+        imageSRC={Picture}
+        title={"نست"}
+        description={"آموزشگاه در تدارک دوره های جدید برنامه نویسی است."}
+        category={"آموزشی"}
+      />
+      ُ
+    </div>
+  );
 }
 
 export default App;
