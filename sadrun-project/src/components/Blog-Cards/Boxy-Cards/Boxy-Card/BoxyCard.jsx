48488f16cf61119186dfe2d494283b7b3137af3e
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-09 19:50:07 +0330

    boxyCard styling completed

--- sadrun-project/src/components/Blog-Cards/Boxy-Cards/Boxy-Card/BoxyCard.jsx
+++ sadrun-project/src/components/Blog-Cards/Boxy-Cards/Boxy-Card/BoxyCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import "../../../../sass/fonts.scss"
 import styles from "./BoxyCard.module.scss";
+import categoryPin from "../images/category-pin.svg"
 
 function BoxyCard({ imageSRC, title, description, category }) {
     return <div className={styles.boxyCard}>
@@ -7,7 +9,9 @@ function BoxyCard({ imageSRC, title, description, category }) {
         <h4 className={styles.cardTitle}>{title}</h4>
         <h6 className={styles.cardDetails}>{description}</h6>
         <div className={styles.bottomPart}>
-            <span className={styles.cardCategory}>{category}</span>
+            <span className={styles.cardCategory}>
+                <img src={categoryPin} alt="" />
+                {category}</span>
             <button className={styles.cardButton}>ادامه مطلب</button>
         </div>
     </div>;
