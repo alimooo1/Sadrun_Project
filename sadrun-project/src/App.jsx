178d2c0fb740e1a2664d2e62b6c0ed1d18cf9266
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-04-09 12:17:35 +0330

    adding content and a liitle css

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import QA from "./components/Q&A/QA";
+import Footer from "./components/Footer/Footer";
+import MapFooter from "./components/Map-Footer/MapFooter";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <MapFooter/>
+  </div>;
 }
 
 export default App;
