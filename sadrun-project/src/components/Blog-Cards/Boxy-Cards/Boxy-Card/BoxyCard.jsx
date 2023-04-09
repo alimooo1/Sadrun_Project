aabdec2561446086fb1cca880c959c85fa5ed4f6
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-09 10:40:38 +0330

    BoxyCard.jsx developed

--- sadrun-project/src/components/Blog-Cards/Boxy-Cards/Boxy-Card/BoxyCard.jsx
+++ sadrun-project/src/components/Blog-Cards/Boxy-Cards/Boxy-Card/BoxyCard.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import "./BoxyCard.module.scss";
+import styles from "./BoxyCard.module.scss";
 
-export default function BoxyCard() {
-  return <div></div>;
+function BoxyCard({ imageSRC, title, description, category }) {
+    return <div className={styles.boxyCard}>
+        <img src={imageSRC} className={styles.cardImage} alt="" />
+        <h4 className={styles.cardTitle}>{title}</h4>
+        <h6 className={styles.cardDetails}>{description}</h6>
+        <div className={styles.bottomPart}>
+            <span className={styles.cardCategory}>{category}</span>
+            <button className={styles.cardButton}>ادامه مطلب</button>
+        </div>
+    </div>;
 }
+
+export default BoxyCard;
