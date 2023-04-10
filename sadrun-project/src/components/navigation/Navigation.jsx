0e4885d6c194c0b7eb5026cee426806195b6b83c
masumeh <khanimasume88@gmail.com>
2023-04-10 11:14:05 +0430

    complete right side of navigation

--- sadrun-project/src/components/navigation/Navigation.jsx
+++ sadrun-project/src/components/navigation/Navigation.jsx
@@ -6,8 +6,8 @@ const Navigation = () => {
         <div className={styles.navigation}>
             <div class="navigation-logo"><a href="#"> <img src={avatar} alt="avatar" /></a></div>
             <nav>
-                <ul className='navigation-items'>
-                    <li className='navigation-item'><a href="#">خانه</a></li>
+                <ul className={styles.navigationItems}>
+                    <li className={styles.navigationItem}><a href="#">خانه</a></li>
                     <li><a href="#">آموزش</a></li>
                     <li><a href="#">بلاگ</a></li>
                     <li><a href="#">خدمات</a></li>
