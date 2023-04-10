439a67aca0a16347a0ac2da758d6e0b1bee3bc0b
masumeh <khanimasume88@gmail.com>
2023-04-10 10:25:30 +0430

    delete axtra images from landing page images

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import QA from "./components/Q&A/QA";
 import ServiceCard from "./components/ServiceCart/ServiceCard";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return <div className="App">
-    <ServiceCard />
+    <LandingPage />
   </div>;
 }
 
