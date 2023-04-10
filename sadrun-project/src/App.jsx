8999cb74443947143077bc2aa64e55c83803d302
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-04-11 03:15:00 +0330

    some element added.

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import QA from "./components/Q&A/QA";
+import CD from "./components/C&D/CD";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <CD/>
+  </div>;
 }
 
 export default App;
--- sadrun-project/src/components/C&D/CD.jsx
+++ sadrun-project/src/components/C&D/CD.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 import "../../sass/fonts.scss";
 import styles from "./style.module.scss";
-
-
+import nodePic from "./assets/node.png";
 
 export default function CD() {
+  const [visible, setVisible] = useState("discription");
+
   return (
-    <div></div>
-  )
+    <div className={styles.main_card}>
+      <img src={nodePic} alt="" />
+      <h1 className={styles.title}>نود جی اس</h1>
+      <div className={styles.tab}>
+        {/* ----------Tab Links---------------  */}
+        <div className={styles.tablinks}>
+          <button className={styles.active_btn}>توضیحات</button>
+          <button>نظرات</button>
+        </div>
+
+        {/* ----------Tab Content---------------  */}
+        <div className={styles.tabcontent}>
+          <div className={styles.active_content}>
+            <h3>Descriptions</h3>
+          </div>
+
+          <div>
+            <h3>Comments</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
