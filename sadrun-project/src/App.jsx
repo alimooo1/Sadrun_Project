fcbbd4db953874a331da48306a46614617dec384
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-11 03:08:29 +0430

    contact_us component(jsx,image,css)

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import QA from "./components/Q&A/QA";
+import Contact_us from "./components/contact_us/contact_us";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Contact_us/>
+  </div>;
 }
 
 export default App;
