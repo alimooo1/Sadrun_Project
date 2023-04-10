98060fbae73a615ae56575172c7faf371efa8e2b
masumeh <khanimasume88@gmail.com>
2023-04-10 16:09:43 +0430

    install react icons and import it to component

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import QA from "./components/Q&A/QA";
 import ServiceCard from "./components/ServiceCart/ServiceCard";
+import CourseBenefits from "./components/CourseBenefits/CourseBenefits";
 
 function App() {
   return <div className="App">
-    <ServiceCard />
+    <CourseBenefits />
+    fff
   </div>;
 }
 
---
+++ sadrun-project/src/components/CourseBenefits/CourseBenefits.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BsFillCheckSquareFill } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+const CourseBenefits = () => {
+    return (
+        <div>
+            {/* <FaHeart /> */}
+            <p>aslam</p>
+            salam
+            {/* <BsFillCheckSquareFill /> */}
+
+        </div>
+    )
+}
+
+export default CourseBenefits
