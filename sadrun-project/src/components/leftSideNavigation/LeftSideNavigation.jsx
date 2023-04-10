3c4cd0d2740010d1bb8de5d9490c3184d1fe1249
masumeh <khanimasume88@gmail.com>
2023-04-10 22:49:18 +0430

    add style to leftSideNavigation component

--- sadrun-project/src/components/leftSideNavigation/LeftSideNavigation.jsx
+++ sadrun-project/src/components/leftSideNavigation/LeftSideNavigation.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import styles from "../Creat Btn Component/CreateBtnComponent.module.css"
+import shoppinIcon from "./image/Path 619.svg";
+import searchIcon from "./image/Path 761.svg";
 
-const LeftSideNavigation = () => {
+function CreateBtnComponent() {
     return (
-        <div>
+        <div className={styles.conteiner} >
+
+            <button className={styles.search}>
+                <img className={styles.searchIcon} src={searchIcon} alt="" />
+            </button>
+            <button className={styles.login}>ورود</button>
+            <button className={styles.register}>ثبت نام</button>
+            <button className={styles.shopping_cart}>
+                <img className={styles.shoppinIcon} src={shoppinIcon} alt="shoppinIcon" />
+            </button>
+
 
         </div>
     )
 }
 
-export default LeftSideNavigation
+export default CreateBtnComponent
