f7ba832debc74c17cf25c3fc488e3b2f51cd7d27
Javad Hosseini <you@example.com>
2023-04-11 18:07:57 +0330

    component completed.

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import CategoryPin from './components/Category-Pin/CategoryPin';
 
 function App() {
   return <div className="App">
-    <CategoryPin name={"آموزشی"}/>
   </div>;
 }
 
