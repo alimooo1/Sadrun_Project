6b6a85094a30a5eac0937c721270eb80962781fb
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-11 17:37:49 +0430

    update style component ContactUS

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import QA from "./components/Q&A/QA";
 import ServiceCard from "./components/ServiceCart/ServiceCard";
 import StarRating from "./components/StarRating/StarRating";
+import Contact_us from "./components/contact_us/contact_us";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Contact_us/>
+  </div>;
 }
 
 export default App;
