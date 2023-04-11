ebccd0d1df2adbfd5f778383f210a991bd9e61e0
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-04-11 05:34:11 +0330

    component starRating added.

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import QA from "./components/Q&A/QA";
 import ServiceCard from "./components/ServiceCart/ServiceCard";
+import StarRating from "./components/StarRating/StarRating";
 
 function App() {
   return <div className="App">
-    <ServiceCard />
+    <StarRating/>
   </div>;
 }
 
---
+++ sadrun-project/src/components/StarRating/StarRating.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StarRating = () => {
+  return (
+    <div>StarRating</div>
+  )
+}
+
+export default StarRating
