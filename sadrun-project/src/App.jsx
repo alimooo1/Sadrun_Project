0e1cee7f955b1af70b448d14f165a218176e74f6
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-11 18:52:45 +0330

    additional imports removed from App

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Footer from "./components/Footer/Footer";
-import QA from "./components/Q&A/QA";
-import MPCourses from "./components/Main-page-courses/MPCourses";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
