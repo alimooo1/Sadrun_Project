fafb8fc2b14e3ec06e8c52ce377bc08bbad6cbf1
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-11 13:28:07 +0330

    RowCard.jsx developed

--- sadrun-project/src/components/Blog-Cards/Row-Cards/Row-Card/RowCard.jsx
+++ sadrun-project/src/components/Blog-Cards/Row-Cards/Row-Card/RowCard.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import styles from "./RowCard.module.scss";
+import CategoryPin from "../../Category-Pin/CategoryPin";
 
-export default function RowCard() {
-  return <div></div>;
+export default function RowCard({ imageSRC, title, details, category }) {
+  return <div className={styles.rowCard}>
+    <img src={imageSRC} alt="" />
+    <div className={styles.leftPart}>
+      <div className={styles.leftTopPart}>
+        <h4 className={styles.cardTitle}>{title}</h4>
+        <h6 className={styles.cardDetail}>{details}</h6>
+      </div>
+      <div className={styles.leftBottomPart}>
+        <CategoryPin name={category} />
+      </div>
+
+    </div>
+  </div>;
 }
