001e7ea9d8484d516f58a03c6389239a43d2ff14
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-04-11 04:58:41 +0330

    styling improved and tab functionality added.

--- sadrun-project/src/components/C&D/CD.jsx
+++ sadrun-project/src/components/C&D/CD.jsx
@@ -4,8 +4,11 @@ import styles from "./style.module.scss";
 import nodePic from "./assets/node.png";
 
 export default function CD() {
-  const [visible, setVisible] = useState("discription");
+  const [toggleState, setToggleState] = useState(1);
 
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <div className={styles.main_card}>
       <img src={nodePic} alt="" />
@@ -13,17 +16,61 @@ export default function CD() {
       <div className={styles.tab}>
         {/* ----------Tab Links---------------  */}
         <div className={styles.tablinks}>
-          <button className={styles.active_btn}>توضیحات</button>
-          <button>نظرات</button>
+          <div
+            className={
+              toggleState === 1
+                ? `${styles.active_link} ${styles.description_btn}`
+                : styles.description_btn
+            }
+            onClick={() => toggleTab(1)}
+          >
+            توضیحات
+          </div>
+          <div
+            className={
+              toggleState === 2
+                ? `${styles.active_link} ${styles.comments_btn}`
+                : styles.comments_btn
+            }
+            onClick={() => toggleTab(2)}
+          >
+            نظرات
+            <span>14</span>
+          </div>
         </div>
 
         {/* ----------Tab Content---------------  */}
         <div className={styles.tabcontent}>
-          <div className={styles.active_content}>
-            <h3>Descriptions</h3>
+          <div
+            className={
+              toggleState === 1
+                ? `${styles.active_content} ${styles.description_content}`
+                : styles.description_content
+            }
+          >
+            <p>
+              نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ
+              سمت سرورِ متن‌باز و چندسکویی برای زبان جاوااسکریپت است.نود جی‌اس
+              یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ سمت سرورِ
+              متن‌باز و چندسکویی برای زبان جاوااسکریپت است.نود جی‌اس یا نود دات
+              جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ سمت سرورِ متن‌باز و
+              چندسکویی برای .زبان جاوااسکریپت است نود جی‌اس یا نود دات جی‌اس (به
+              انگلیسی: Node.js) یک محیط اجراییِ سمت سرورِ متن‌باز و چندسکویی
+              برای زبان جاوااسکریپت است.نود جی‌اس یا نود دات جی‌اس (به انگلیسی:
+              Node.js) یک محیط اجراییِ سمت سرورِ متن‌باز و چندسکویی برای زبان
+              جاوااسکریپت است نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js)
+              یک محیط اجراییِ سمت سرورِ متن‌باز و چندسکویی برای زبان جاوااسکریپت
+              است.نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط
+              اجراییِ سمت سرورِ متن‌باز و .چندسکویی برای زبان جاوااسکریپت است.
+            </p>
+            <div className={styles.info}>abc</div>
           </div>
 
-          <div>
+          <div className={
+              toggleState === 2
+                ? `${styles.active_content} ${styles.description_content}`
+                : styles.comment_content
+            }>
             <h3>Comments</h3>
           </div>
         </div>
