942ab71d78e0d942ec0aa5d8e8c39b6c3c7797e0
masumeh <khanimasume88@gmail.com>
2023-04-12 18:50:39 +0430

    Change the name of the TableRow component

--- sadrun-project/src/components/TableDashboard/TableDashboard.jsx
+++ sadrun-project/src/components/TableDashboard/TableDashboard.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
-import styles from './TableDashboard.module.scss'
-const TableDashboard = (props) => {
+import styles from './TableDashboard.module.scss';
+import TableRow from './TableRow/TableRow';
+const TableDashboard = () => {
+
+
+    const courses = [
+        {
+            id: Math.floor(Math.random()),
+            courseImage: 2,
+            courseName: 'maryam',
+            courseMentorsName: 'ali',
+            courseSeasonName: 'bahar',
+            courseStartDate: 'today',
+            courseEndDate: 'tomorrow',
+            coursePrice: '5000'
+        },
+        {
+            id: Math.floor(Math.random()),
+            courseImage: 2,
+            courseName: 'maryam',
+            courseMentorsName: 'ali',
+            courseSeasonName: 'bahar',
+            courseStartDate: 'today',
+            courseEndDate: 'tomorrow',
+            coursePrice: '5000'
+        },
+        {
+            id: Math.floor(Math.random()),
+            courseImage: 2,
+            courseName: 'maryam',
+            courseMentorsName: 'ali',
+            courseSeasonName: 'bahar',
+            courseStartDate: 'today',
+            courseEndDate: 'tomorrow',
+            coursePrice: '5000'
+        },
+    ]
     return (
         <div className={styles.tableCountainer}>
+            <ul className={styles.courseItemsTitle}>
+                <li></li>
+                <li >نام دوره </li>
+                <li>نام استاد</li>
+                <li>نام ترم </li>
+                <li>تاریخ شروع</li>
+                <li>تاریخ پایان</li>
+                <li>قیمت </li>
+            </ul>
+            {
+                courses.map(courseItem => {
+                    return (
+
+                        <TableRow className={styles.courseItems} key={courseItem.id} courseImage={courseItem.courseImage} courseName={courseItem.courseName} courseMentorsName={courseItem.courseMentorsName} courseSeasonName={courseItem.courseSeasonName} courseStartDate={courseItem.courseStartDate} courseEndDate={courseItem.courseEndDate} coursePrice={courseItem.coursePrice} />
+
+                    )
+                })
+            }
 
         </div>
     )
--- sadrun-project/src/components/TableDashboard/TableRow/TAbleRow.jsx
+++ sadrun-project/src/components/TableDashboard/TableRow/TAbleRow.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import styles from './TAbleRow.module.scss';
+import styles from './TableRow.module.scss';
 const TAbleRow = (props) => {
     return (
         <div className={styles.tableRow}>
             <ul className={styles.courseItems}>
-                <li className={styles.courseImage}></li>
-                <li className={styles.courseName}></li>
-                <li className={styles.courseSeasonName}></li>
-                <li className={styles.courseStartDate}></li>
-                <li></li>
-                <li className={styles.coursePrice} ></li>
-                <li></li>
+                <li >{props.courseImage}</li>
+                <li >{props.courseName}</li>
+                <li >{props.courseMentorsName}</li>
+                <li >{props.courseSeasonName}</li>
+                <li >{props.courseStartDate}</li>
+                <li >{props.courseEndDate}</li>
+                <li >{props.coursePrice}</li>
             </ul>
         </div>
     )
 }
 
 export default TAbleRow
+// 
