37e3e93f4bb016e611b70ac485a4582a45bc8f95
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-13 15:04:07 +0330

    TeacherCard structure bug fixed

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import "./sass/_fonts.scss"
-import CreatTeacherCard from "./components/createTeacheCard/CreatTeacherCard";
 
 function App() {
-  return <div className="App">
-    <CreatTeacherCard/>
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
