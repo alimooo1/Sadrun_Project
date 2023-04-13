5978ccf8a5d45bf33b3bc9ca76b8fb42993f4a4e
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-13 16:31:57 +0330

    additional imports removed from App

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import TeacherCard from "./components/TeacherCard/TeacherCard";
 
 function App() {
-  return <div className="App">
-    <TeacherCard />
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
