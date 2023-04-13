255ec5c527f718ad8b9a9a318e664dedcb3ac2ba
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-13 11:52:15 +0330

    transition & font added to input

--- sadrun-project/src/components/leftSideNavigation/SearchInput/SearchInput.jsx
+++ sadrun-project/src/components/leftSideNavigation/SearchInput/SearchInput.jsx
@@ -1,24 +1,30 @@
-
-import styles from './SearchInput.module.scss';
-import { FaSistrix } from 'react-icons/fa';
-import React, { useState } from 'react';
-
+import styles from "./SearchInput.module.scss";
+import { FaSistrix } from "react-icons/fa";
+import React, { useState } from "react";
+import "../../../sass/fonts.scss";
 
 function InputWithIcon() {
-    const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
-    const handleInputClick = () => {
-        setExpanded(true);
-    };
+  const handleInputClick = () => {
+    setExpanded(true);
+  };
 
-    return (
-        <div className={`${styles.inputContainer} ${expanded ? styles.expanded : ''}`}>
-
-
-            {!expanded && <span onClick={handleInputClick} className={styles.icon}><FaSistrix /></span>}
-            <input type="search" className={`${styles.input} ${expanded ? styles.visible : ''}`} />
-        </div>
-    );
+  return (
+    <div
+      className={`${styles.inputContainer} ${expanded ? styles.expanded : ""}`}
+    >
+      {!expanded && (
+        <span onClick={handleInputClick} className={styles.icon}>
+          <FaSistrix />
+        </span>
+      )}
+      <input
+        type="search"
+        className={`${styles.input} ${expanded ? styles.visible : ""}`}
+      />
+    </div>
+  );
 }
 
 export default InputWithIcon;
