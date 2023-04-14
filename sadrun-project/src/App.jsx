e8b5ba31223e446bc7bae44979bc324affc8a93f
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-14 16:34:47 +0330

    additional imports removed from App

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Navigation from "./components/Navigation/Navigation";
 
 function App() {
-  return <div className="App">
-    <Navigation />
-    {/* <LeftSideNaviigation /> */}
-    {/* <SearchInput /> */}
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
