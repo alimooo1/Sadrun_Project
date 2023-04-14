f03340e33add539fda6e0498813d5ef0f4a880b3
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-14 17:19:37 +0330

    prettier code for Teacher card

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import CreatTeacherCard from "./components/TeacherCard/TeacherCard";
 
 function App() {
-  return <div className="App">
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
