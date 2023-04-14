9930e5f0610027906929e9692f0a0d2961cd3443
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-14 17:23:26 +0330

    ServiceCard's implementation removed from App

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import QA from "./components/Q&A/QA";
-import ServiceCard from "./components/ServiceCart/ServiceCard";
 
 function App() {
   return (
-    <div className="App">
-      {/* <div style={{ maxWidth: 1040, margin: "auto", border: "1px solid red" }}> */}
-      <ServiceCard />
-      {/* </div> */}
-    </div>
+    <div className="App"></div>
   );
 }
 
