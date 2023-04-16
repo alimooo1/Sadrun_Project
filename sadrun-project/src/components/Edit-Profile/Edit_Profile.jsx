e112b2b6de9e1cd7cf753a490dbc7fa7573821c5
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-16 19:25:08 +0430

    add editeprofileinputeComponent  in upadate style

--- sadrun-project/src/components/Edit-Profile/Edit_Profile.jsx
+++ sadrun-project/src/components/Edit-Profile/Edit_Profile.jsx
@@ -1,64 +1,45 @@
-import React from 'react'
-import "../../sass/fonts.scss"
-import styles from "./Edit_Profile.module.scss"
-import Edite_icon from "./image/user-edit-svgre-mdpi.svg"
-import user_image from "./image/photo_2020-11-1-mdpi.png"
+import React, { useState } from "react";
+import "../../sass/fonts.scss";
+import styles from "./Edit_Profile.module.scss";
+import user_image from "./image/photo_2020-11-1-mdpi.png";
+import Edit_Profile_input from "./Edit_Profile_input/Edit_Profile_input";
+
+const inputList = [
+  {
+    text: "نام کاربری",
+  },
+  {
+    text: "رمز عبور ",
+  },
+  {
+    text: "ایمیل",
+  },
+  {
+    text: "کد ملی ",
+  },
+  {
+    text: "تاریخ تولد ",
+  },
+  {
+    text: "شماره همراه ",
+  },
+];
+
 function Edit_Profile() {
   return (
     <div className={styles.conteiner}>
-            <div className={styles.userImage_Conteiner}>
-                <img className={styles.user_image} src={user_image} alt="" />
-            </div>
-        <div className={styles.formConteiner} >
-
-            <form  className={styles.form} action="">
-                <div className={styles.conteiner_input}>
-                    <label htmlFor="">نام کاربری</label>
-                    <div>
-                       <input type="text"  />
-                       <img className={styles.editeIcon} src={Edite_icon} alt="" />
-                    </div>
-                    
-                </div>
-                <div className={styles.conteiner_input}>
-                    <label htmlFor="">رمز عبور</label>
-                    <div>
-                      <input type="password" /> 
-                      <img className={styles.editeIcon}  src={Edite_icon} alt="" />  
-                    </div>
-                </div>
-                <div className={styles.conteiner_input}>
-                    <label htmlFor=""> ایمیل</label>
-                    <div>
-                      <input type="email" />  
-                      <img className={styles.editeIcon} src={Edite_icon} alt="" />  
-                    </div>
-                </div>
-                <div className={styles.conteiner_input}>
-                    <label htmlFor=""> کد ملی</label>
-                    <div>
-                      <input type="text" />  
-                      <img className={styles.editeIcon} src={Edite_icon} alt="" />  
-                    </div>
-                </div>
-                <div className={styles.conteiner_input}>
-                    <label htmlFor=""> تاریخ تولد</label>
-                    <div>
-                      <input type="text" />  
-                      <img className={styles.editeIcon} src={Edite_icon} alt="" />  
-                    </div>
-                </div>
-                <div className={styles.conteiner_input}>
-                    <label htmlFor="">شماره همراه</label>
-                    <div>
-                      <input type="text" />  
-                      <img className={styles.editeIcon} src={Edite_icon} alt="" />  
-                    </div>
-                </div>
-            </form>
-        </div>
+      <div className={styles.userImage_Conteiner}>
+        <img className={styles.user_image} src={user_image} alt="" />
+      </div>
+      <div className={styles.formConteiner}>
+        <form className={styles.form} action="">
+          {inputList.map((edit) => {
+            return <Edit_Profile_input edit={edit} />;
+          })}
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Edit_Profile
+export default Edit_Profile;
---
+++ sadrun-project/src/components/Edit-Profile/Edit_Profile_input/Edit_Profile_input.jsx
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from "react";
+import styles from "../Edit_Profile_input/Edit_Profile_input.module.scss";
+import Edite_icon from "../image/user-edit-svgre-mdpi.svg";
+import Tick from "../image/iconmonstr-check-mark-17.svg";
+
+function Edit_Profile_input(props, onSubmit) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const inputRef = useRef();
+
+  const handleSubmit = () => {
+    setIsEditing(false);
+    onSubmit(inputRef.current.value);
+  };
+
+  return (
+    <div className={styles.conteiner_input}>
+      <label htmlFor="">{props.edit.text} </label>
+      <div>
+        <input
+          ref={inputRef}
+          disabled={!isEditing}
+          type="text"
+          defaultValue={props.edit.value}
+        />
+        {!isEditing ? (
+          <img
+            className={styles.editeIcon}
+            onClick={() => setIsEditing(true)}
+            src={Edite_icon}
+            alt=""
+          />
+        ) : (
+          <img
+            className={styles.editeIcon}
+            onClick={handleSubmit}
+            src={Tick}
+            alt=""
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Edit_Profile_input;
