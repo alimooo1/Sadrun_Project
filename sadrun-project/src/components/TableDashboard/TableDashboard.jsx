d4d4f4781c477a82c2577d0742c2e3772087ae79
masumeh <khanimasume88@gmail.com>
2023-04-16 09:45:56 +0430

    modify sizes in TableRow & TableDashboard

--- sadrun-project/src/components/TableDashboard/TableDashboard.jsx
+++ sadrun-project/src/components/TableDashboard/TableDashboard.jsx
@@ -7,113 +7,115 @@ const TableDashboard = () => {
 
 
     const courses = [
-        {
-            id: Math.floor(Math.random()),
-            src: courseImage,
-            courseName: 'maryam',
-            courseMentorsName: 'ali',
-            courseSeasonName: 'bahar',
-            courseStartDate: 'today',
-            courseEndDate: 'tomorrow',
-            coursePrice: '5000'
-        },
-        {
-            id: Math.floor(Math.random()),
-            src: courseImage,
-            courseName: 'maryam',
-            courseMentorsName: 'ali',
-            courseSeasonName: 'bahar',
-            courseStartDate: 'today',
-            courseEndDate: 'tomorrow',
-            coursePrice: '5000'
-        },
-        {
-            id: Math.floor(Math.random()),
-            src: courseImage,
-            courseName: 'maryam',
-            courseMentorsName: 'ali',
-            courseSeasonName: 'bahar',
-            courseStartDate: 'today',
-            courseEndDate: 'tomorrow',
-            coursePrice: '5000'
-        },
-        {
-            id: Math.floor(Math.random()),
-            src: courseImage,
-            courseName: 'maryam',
-            courseMentorsName: 'ali',
-            courseSeasonName: 'bahar',
-            courseStartDate: 'today',
-            courseEndDate: 'tomorrow',
-            coursePrice: '5000'
-        },
-        {
-            id: Math.floor(Math.random()),
-            src: courseImage,
-            courseName: 'maryam',
-            courseMentorsName: 'ali',
-            courseSeasonName: 'bahar',
-            courseStartDate: 'today',
-            courseEndDate: 'tomorrow',
-            coursePrice: '5000'
-        },
-        {
-            id: Math.floor(Math.random()),
-            src: courseImage,
-            courseName: 'maryam',
-            courseMentorsName: 'ali',
-            courseSeasonName: 'bahar',
-            courseStartDate: 'today',
-            courseEndDate: 'tomorrow',
-            coursePrice: '5000'
-        },
-    ]
+      {
+        id: Math.floor(Math.random()),
+        src: courseImage,
+        courseName: "maryam",
+        courseMentorsName: "mahsa",
+        courseSeasonName: "bahar",
+        courseStartDate: "today",
+        courseEndDate: "tomorrow",
+        coursePrice: "5000",
+      },
+      {
+        id: Math.floor(Math.random()),
+        src: courseImage,
+        courseName: "sara",
+        courseMentorsName: "ali",
+        courseSeasonName: "bahar",
+        courseStartDate: "today",
+        courseEndDate: "tomorrow",
+        coursePrice: "5000",
+      },
+      {
+        id: Math.floor(Math.random()),
+        src: courseImage,
+        courseName: "maryam",
+        courseMentorsName: "neda",
+        courseSeasonName: "bahar",
+        courseStartDate: "today",
+        courseEndDate: "tomorrow",
+        coursePrice: "5000",
+      },
+      {
+        id: Math.floor(Math.random()),
+        src: courseImage,
+        courseName: "maryam",
+        courseMentorsName: "ali",
+        courseSeasonName: "bahar",
+        courseStartDate: "today",
+        courseEndDate: "tomorrow",
+        coursePrice: "5000",
+      },
+      {
+        id: Math.floor(Math.random()),
+        src: courseImage,
+        courseName: "sara",
+        courseMentorsName: "ali",
+        courseSeasonName: "bahar",
+        courseStartDate: "today",
+        courseEndDate: "tomorrow",
+        coursePrice: "5000",
+      },
+      {
+        id: Math.floor(Math.random()),
+        src: courseImage,
+        courseName: "kati",
+        courseMentorsName: "reza",
+        courseSeasonName: "tabestan",
+        courseStartDate: "today",
+        courseEndDate: "tomorrow",
+        coursePrice: "5000",
+      },
+    ];
     return (
+      <div className={styles.Countainer}>
+        <div className={styles.tableHeader}>
+          <div className={styles.registeredCourseNumbers}>
+            <span>دوره های ثبت شده: 26 تا 39 </span>
+          </div>
+          <div className={styles.inputSearchCountainer}>
+            <span className={styles.icon}>
+              <FaSistrix />
+            </span>
+            <input
+              type="search"
+              placeholder="جست و جو "
+              className={styles.input}
+            />
+          </div>
+        </div>
+        <div className={styles.tableCountainer}>
+          <div className={styles.tablrRowTitle}>
+            <ul className={styles.courseItemsTitle}>
+              <li></li>
+              <li>نام دوره </li>
+              <li>نام استاد</li>
+              <li>نام ترم </li>
+              <li>تاریخ شروع</li>
+              <li>تاریخ پایان</li>
+              <li>قیمت </li>
+            </ul>
+          </div>
 
-
-
-
-
-
-        < div className={styles.Countainer} >
-            <div className={styles.tableHeader}>
-                <div className={styles.registeredCourseNumbers}>
-                    <span>دوره های ثبت شده:  26 تا 39 </span>
-                </div>
-                <div className={styles.inputSearchCountainer}>
-                    <span className={styles.icon}><FaSistrix /></span>
-                    <input type="search" placeholder='جست و جو ' className={styles.input} />
-                </div>
-
-            </div>
-            <div className={styles.tableCountainer}>
-                <div className={styles.tablrRowTitle}>
-                    <ul className={styles.courseItemsTitle}>
-                        <li></li>
-                        <li >نام دوره </li>
-                        <li>نام استاد</li>
-                        <li>نام ترم </li>
-                        <li>تاریخ شروع</li>
-                        <li>تاریخ پایان</li>
-                        <li>قیمت </li>
-                    </ul>
-                </div>
-
-                {
-                    courses.map(courseItem => {
-                        return (
-
-                            <TableRow className={styles.courseItems} key={courseItem.id} courseImage={courseItem.src} courseName={courseItem.courseName} courseMentorsName={courseItem.courseMentorsName} courseSeasonName={courseItem.courseSeasonName} courseStartDate={courseItem.courseStartDate} courseEndDate={courseItem.courseEndDate} coursePrice={courseItem.coursePrice} />
-
-                        )
-                    })
-                }
-
-            </div>
-
-        </div >
-
-    )
+          {courses.map((courseItem) => {
+            return (
+              <TableRow
+                className={styles.courseItems}
+                key={courseItem.id}
+                courseImage={courseItem.src}
+                courseName={courseItem.courseName}
+                courseMentorsName={courseItem.courseMentorsName}
+                courseSeasonName={courseItem.courseSeasonName}
+                courseStartDate={courseItem.courseStartDate}
+                courseEndDate={courseItem.courseEndDate}
+                coursePrice={courseItem.coursePrice}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
 }
 
 export default TableDashboard
