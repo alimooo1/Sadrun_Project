867f22438cebee41c1c6a508de3947012d0b9d10
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-17 19:40:31 +0430

    update style

--- sadrun-project/src/components/blogsComponent/BlogsComponet.jsx
+++ sadrun-project/src/components/blogsComponent/BlogsComponet.jsx
@@ -1,18 +1,62 @@
 import React, { useState } from "react";
+import styles from "./BlogsComponet.module.css"
 import BlogComponet from "../blogComponet/BlogComponet";
-// import vs from "./image/visual-studio-logo.png"
+import menuu from "../blogComponet/image/menuu.svg"
 function BlogsComponet() {
-  // const [dataBloge, setDataBloge] = useState({img:"jk",title:"kjj",text:"knn"})
+  // const [dataBloge, setDataBloge] = useState({img:"jk",title:"نود جی س",text:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "})
   const [dataBloge, setDataBloge] = useState([{
     img: "./image/visual-studio-logo.png",
-    title: "kjj",
-    text: "knn",
-  }]);
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },{
+    img: "./image/visual-studio-logo.png",
+    title: "نود جی س",
+    text: "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+  },]);
   return (
-    <div>
-      {dataBloge.map((data) => (
-        <BlogComponet data={data} />
+    <div className={styles.conteiner}>
+    <div className={styles.head}>
+      <div className={styles.headIcon}><img className={styles.iconImg} src={menuu} alt="" /></div>
+      <div className={styles.headText}>بلاگ ها</div>
+    </div>
+    <div className={styles.blogs}>
+      {dataBloge.map((data, index) => (
+        <BlogComponet key={index} data={data} />
       ))}
+    </div>
+      
     </div>
   );
 }
