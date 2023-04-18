5b8ab8f65004b48b567486228b0d0ae113f6d998
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-18 16:35:17 +0330

    LandingLayout developed

--- sadrun-project/src/layouts/Landing-Layout/LandingLayout.jsx
+++ sadrun-project/src/layouts/Landing-Layout/LandingLayout.jsx
@@ -4,6 +4,10 @@ import LandingPage from "../../components/LandingPage/LandingPage"
 import Services from "../../components/ServiceCart/ServiceCard"
 import Categories from "../../components/CategoryCard/CategoryCard"
 import Courses from "../../components/Main-page-courses/MPCourses"
+import Teachers from "../../components/TeacherCard/TeacherCard"
+import Articles from "../../components/Article-Card/ArticleCard"
+import QA from "../../components/Q&A/QA"
+import Footer from "../../components/Map-Footer/MapFooter"
 
 export default function LandingLayout() {
   return (
@@ -15,13 +19,25 @@ export default function LandingLayout() {
         <section className={styles.services}>
           <Services />
         </section>
-        <section className={styles.category}>
+        <section className={styles.categories}>
           <Categories />
         </section>
         <section className={styles.courses}>
           <Courses />
         </section>
+        <section className={styles.teachers}>
+          <Teachers />
+        </section>
+        <section className={styles.articles}>
+          <Articles />
+        </section>
+        <section className={styles.QA}>
+          <QA />
+        </section>
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
