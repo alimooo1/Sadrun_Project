3b65641b1d235d676099c17655c3e98677161db8
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-20 19:34:46 +0430

    update style (respansive)

--- sadrun-project/src/components/Edit-Profile/Edit_Profile.jsx
+++ sadrun-project/src/components/Edit-Profile/Edit_Profile.jsx
@@ -3,19 +3,17 @@ import "../../sass/fonts.scss";
 import styles from "./Edit_Profile.module.scss";
 import user_image from "./image/photo_2020-11-1-mdpi.png";
 import Edit_Profile_input from "./Edit_Profile_input/Edit_Profile_input";
-import axios from 'axios'
+import axios from "axios";
 // useEffect(() => {
-  async function data(){
-    try {
-      const datas= axios.put("http://185.94.96.76:8082/api/auth/login")
-      console.log(datas);
-      
-    } catch (error) {
-      console.log(error);
-    }
+async function data() {
+  try {
+    const datas = axios.put("http://185.94.96.76:8082/api/auth/login");
+    console.log(datas);
+  } catch (error) {
+    console.log(error);
+  }
 }
 // }, [])
-
 
 const inputList = [
   {
@@ -46,7 +44,7 @@ function Edit_Profile() {
       </div>
       <div className={styles.formConteiner}>
         <form className={styles.form} action="">
-          {inputList.map((edit,index ) => {
+          {inputList.map((edit, index) => {
             return <Edit_Profile_input key={index} edit={edit} />;
           })}
         </form>
