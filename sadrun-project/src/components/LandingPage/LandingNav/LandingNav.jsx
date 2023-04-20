ff8d3e1bdebaa6f0a8506d102bc397f5afa9dfc1
masumeh <khanimasume88@gmail.com>
2023-04-20 12:26:42 +0430

    add style to nav in landing nav

--- sadrun-project/src/components/LandingPage/LandingNav/LandingNav.jsx
+++ sadrun-project/src/components/LandingPage/LandingNav/LandingNav.jsx
@@ -3,33 +3,37 @@ import styles from "./LandingNav.module.scss";
 import avatar from "./navigation-image/avatar.svg";
 import LeftSideNavigation from "../leftSideNavigation/LeftSideNavigation";
 import { BiMenu } from "react-icons/bi";
+import { useState } from "react";
 
 BiMenu;
 const LandingNav = (props) => {
   // nav display state
-  const [state, setstate] = useState(initialState);
+  const [openMenu, setOpenMenu] = useState(false);
+  const addMenuHandller = () => {
+    setOpenMenu(true);
+  };
 
   return (
     <div className={`${styles.navigation}`}>
       {/* <div class="navigation-logo"><a href="#"> <img src={avatar} alt="avatar" /></a></div> */}
       <nav>
+        <div className={styles.navigationLogo}>
+          <a href="#">
+            {" "}
+            <img src={avatar} alt="avatar" />
+          </a>
+        </div>
+
+        <div className={styles.navigationIcon}>
+          <a href="#">
+            {" "}
+            <BiMenu
+              onClick={addMenuHandller}
+              className={styles.navigationIcon}
+            />
+          </a>
+        </div>
         <ul className={styles.navigationItems}>
-          <div className={styles.navigationLogo}>
-            <a href="#">
-              {" "}
-              <img src={avatar} alt="avatar" />
-            </a>
-          </div>
-          .
-          <div className={styles.navigationIcon}>
-            <a href="#">
-              {" "}
-              <BiMenu
-                onClick={addNavHandller}
-                className={styles.navigationIcon}
-              />
-            </a>
-          </div>
           <li className={styles.navigationItem}>
             <a href="#">خانه</a>
           </li>
