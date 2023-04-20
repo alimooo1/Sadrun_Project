4466cce63bec8460ec434575c0c77e8affe302ef
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-04-20 14:16:58 +0430

    component courseTeacher add(jsx,scss)

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import CourserTeacher from "../src/components/course-Teacher/courseTeacher"
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <CourserTeacher/>
+  </div>;
 }
 
 export default App;
