fd559be52f369f15b353000fb7baebf057205db9
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-20 12:13:26 +0330

    Additional imports removed from App

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import QA from "./components/Q&A/QA";
-import ServiceCard from "./components/ServiceCart/ServiceCard";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <div style={{ maxWidth: 1040, margin: "auto", border: "1px solid red" }}> */}
-      <ServiceCard />
-      {/* </div> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
