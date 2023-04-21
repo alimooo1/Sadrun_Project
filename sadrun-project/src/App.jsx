8700d8d5790b8cecc9eebf26e4ae4acd70d943b7
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-04-21 12:36:46 +0330

    deleting component from app.

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import CoursePrice from './components/Course_price/CoursePrice';
 
 function App() {
-  return <div className="App">
-    <CoursePrice coursPrice={2498000} courseDiscount={2} date={"2024-00-00"}/>
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
