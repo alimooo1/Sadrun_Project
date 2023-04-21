46bea6aca95b5c77b5d686a189ba278ae1ecd808
Javad Hosseini <you@example.com>
2023-04-21 07:01:14 +0330

    elements added and styling improved.

--- sadrun-project/src/components/Course_price/CoursePrice.jsx
+++ sadrun-project/src/components/Course_price/CoursePrice.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import "../../sass/fonts.scss";
+import styles from "./style.module.scss";
 
 export default function CoursePrice() {
   return (
-    <div>CoursePrice</div>
-  )
+    <div className={styles.course_pice}>
+      <div className={styles.row}>
+        <span>قیمت دوره</span>
+        <span className={styles.price}>2498000</span>
+      </div>
+      <div className={styles.row}>
+        <span>با تخفیف</span>
+        <span className={styles.discount}>2398000</span>
+      </div>
+      <div>
+        <span className={styles.time_left}>مهلت استفاده از تخفیف</span>
+      </div>
+      <button className={styles.button}>ثبت نام در دوره</button>
+    </div>
+  );
 }
