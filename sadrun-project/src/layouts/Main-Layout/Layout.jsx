91f9a94c421d8a2f9ce445ebc4a7abee4435f20f
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-21 23:48:12 +0330

    Right Navigation Replaced in Main Layout

--- sadrun-project/src/layouts/Main-Layout/Layout.jsx
+++ sadrun-project/src/layouts/Main-Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navigation from "../../components/navigation/Navigation";
+import Navigation from "../../components/LandingPage/LandingNav/LandingNav";
 import Footer from "../../components/Footer/Footer";
 
 export default function Layout() {
