749eac86471e7dfbf44ee95a27fba38e4cf8228e
Javad Hosseini <you@example.com>
2023-04-21 07:00:21 +0330

    component added to tha App.jsx for test.

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import CoursePrice from './components/Course_price/CoursePrice';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <CoursePrice/>
+  </div>;
 }
 
 export default App;
