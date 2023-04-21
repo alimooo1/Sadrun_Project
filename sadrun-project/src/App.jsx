29acd6850ccfb846df7116873ed6af6690ef280a
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-21 15:23:43 +0330

    ContactUs structure Refactored

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import QA from "./components/Q&A/QA";
-import ServiceCard from "./components/ServiceCart/ServiceCard";
-import StarRating from "./components/StarRating/StarRating";
-import Contact_us from "./components/contact_us/contact_us";
+import Contact_us from "./components/Contact-Us/ContactUs";
 
 function App() {
-  return <div className="App">
-    <Contact_us/>
-  </div>;
+  return (
+    <div className="App">
+      <Contact_us />
+    </div>
+  );
 }
 
 export default App;
