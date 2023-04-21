a805e4c0a0fcb2f72a71014e8550787dadfcfbe9
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-04-21 10:56:53 +0330

    styling completed.

--- sadrun-project/src/components/Course_price/CoursePrice.jsx
+++ sadrun-project/src/components/Course_price/CoursePrice.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "../../sass/fonts.scss";
 import styles from "./style.module.scss";
+import price from "./assets/price.png";
+import discount from "./assets/discount.png";
+import Countdown from "./Countdown/Countdown";
 
 export default function CoursePrice() {
   return (
     <div className={styles.course_pice}>
       <div className={styles.row}>
-        <span>قیمت دوره</span>
+        <div className={styles.title}>
+          <img className={styles.price_img} src={price} alt="" />
+          <span className={styles.price_text}>قیمت دوره</span>
+        </div>
         <span className={styles.price}>2498000</span>
       </div>
       <div className={styles.row}>
-        <span>با تخفیف</span>
+        <div className={styles.title}>
+          <img className={styles.discount_img} src={discount} alt="" />
+          <span className={styles.discount_text}>با تخفیف</span>
+        </div>
         <span className={styles.discount}>2398000</span>
       </div>
       <div>
         <span className={styles.time_left}>مهلت استفاده از تخفیف</span>
+        <Countdown timestampMs={1705807687} />
       </div>
       <button className={styles.button}>ثبت نام در دوره</button>
     </div>
