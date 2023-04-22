04920e88d2e554a6925e3547ea4ac972e835ce32
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-22 20:22:03 +0330

    BuyLevel.jsx developed

--- sadrun-project/src/components/Buy-Level/BuyLevel.jsx
+++ sadrun-project/src/components/Buy-Level/BuyLevel.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./BuyLevel.module.scss";
+import finishedPic from "./images/finished.svg";
+import detailPic from "./images/detail.svg";
+import cardPic from "./images/card.svg";
 
 export default function BuyLevel() {
+
+  const levelHandler = level => {
+    let [detailOn, finishedOn] = [false, false];
+    let [firstProgressOn, secondProgressOn] = [false, false];
+
+    switch (level) {
+      case "2":
+        firstProgressOn = true
+
+    }
+  }
   return (
     <div className={styles.outsideContainer}>
       <div className={styles.insideContainer}>
-        <div className={styles.cart}></div>
-        <div className={styles.detail}></div>
-        <div className={styles.finished}></div>
+        <div className={styles.card}>
+          <img src={cardPic} alt="" />
+          سبد خرید</div>
+        <div className={styles.detail}>
+          <img src={detailPic} alt="" />
+          جزئیات پرداخت</div>
+        <div className={styles.finished}>
+          <img src={finishedPic} alt="" />
+          اتمام فرایند خرید</div>
       </div>
     </div>)
 }
