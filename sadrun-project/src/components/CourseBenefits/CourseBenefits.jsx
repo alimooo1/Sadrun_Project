06f1b0ec339dab3a07ab067d4bf8f0a831193142
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-23 13:24:00 +0330

    direction bug fixed in course benefit

--- sadrun-project/src/components/CourseBenefits/CourseBenefits.jsx
+++ sadrun-project/src/components/CourseBenefits/CourseBenefits.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import tickIcon from './image/icons8-tick-box.png'
-import styles from './CourseBenefits.module.scss'
+import React from "react";
+import tickIcon from "./images/tick.svg";
+import styles from "./CourseBenefits.module.scss";
 const CourseBenefits = () => {
-    return (
-        <div className={styles.benefitsCountainer}>
-            <h2>مزایای شرکت در این دوره</h2>
-            <ul className={styles.benefitsList}>
-                <li>
-                    <span className={styles.itemText}>ارتباط مستقیم با مدرس</span>
-                    <img className={styles.icon} src={tickIcon} alt="tickIcon" />
-                </li>
-                <li>
-                    <span className={styles.itemText}>ساخت رزومه مناسب برای بازار کار</span>
-                    <img className={styles.icon} src={tickIcon} alt="tickIcon" />
-                </li>
-                <li>
-                    <span className={styles.itemText}>معرفی به بهترین شرکت ها</span>
-                    <img className={styles.icon} src={tickIcon} alt="tickIcon" />
-                </li>
-            </ul>
-        </div>
-    )
-}
-export default CourseBenefits
+  return (
+    <div className={styles.benefitsCountainer}>
+      <h2>مزایای شرکت در این دوره</h2>
+      <ul className={styles.benefitsList}>
+        <li>
+          <img className={styles.icon} src={tickIcon} alt="tickIcon" />
+          <span className={styles.itemText}>ارتباط مستقیم با مدرس</span>
+        </li>
+        <li>
+          <img className={styles.icon} src={tickIcon} alt="tickIcon" />
+          <span className={styles.itemText}>
+            ساخت رزومه مناسب برای بازار کار
+          </span>
+        </li>
+        <li>
+          <img className={styles.icon} src={tickIcon} alt="tickIcon" />
+          <span className={styles.itemText}>معرفی به بهترین شرکت ها</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default CourseBenefits;
