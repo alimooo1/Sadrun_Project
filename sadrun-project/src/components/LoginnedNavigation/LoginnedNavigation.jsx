8d498d5bfadbfeb2caba775ed6c6e737b9cca3c4
masumeh <khanimasume88@gmail.com>
2023-04-23 17:30:00 +0430

    create LoginnedNavigation component

---
+++ sadrun-project/src/components/LoginnedNavigation/LoginnedNavigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingNav from "../LandingPage/LandingNav/LandingNav";
+
+const LoginnedNavigation = () => {
+  return (
+    <div>
+      <LandingNav />
+    </div>
+  );
+};
+
+export default LoginnedNavigation;
