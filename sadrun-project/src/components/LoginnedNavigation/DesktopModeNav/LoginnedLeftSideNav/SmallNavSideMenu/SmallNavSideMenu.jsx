8fa9a1cc7da05ef500d2c56bbbd945d0f5d08a95
masumeh <khanimasume88@gmail.com>
2023-04-24 13:01:34 +0430

    complete SmallNavMenu component

--- sadrun-project/src/components/LoginnedNavigation/DesktopModeNav/LoginnedLeftSideNav/SmallNavSideMenu/SmallNavSideMenu.jsx
+++ sadrun-project/src/components/LoginnedNavigation/DesktopModeNav/LoginnedLeftSideNav/SmallNavSideMenu/SmallNavSideMenu.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import { useState } from "react";
+import styles from "./SmallNavSideMenu.module.scss";
+import { FaSistrix, FaUserEdit } from "react-icons/fa";
+import { AiFillCloseSquare } from "react-icons/ai";
+import avatar from "../../../images/avatar.svg";
+import ProPic from "../../../images/stuPic.svg";
+import { FaBars } from "react-icons/fa";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { ImList2 } from "react-icons/im";
+import { BiErrorCircle } from "react-icons/bi";
+import { MdOutlineModelTraining } from "react-icons/md";
+import { IoMdExit } from "react-icons/io";
 
 const SmallNavSideMenu = () => {
-  return <div></div>;
+  const [showIcon, setShowIcon] = useState(true);
+  const changeHandler = () => {
+    setShowIcon(false);
+  };
+  const handleInputClick = () => {
+    console.log("mm");
+  };
+  return (
+    <div className={`${styles.navSideContainer}`}>
+      <div className={styles.navSideHeader}>
+        <img className={styles.navigationLogo} src={avatar} alt="logo" />
+        <span className={styles.navigationCloseIcon}>
+          <AiFillCloseSquare className={styles.navigationIcon} />
+        </span>
+      </div>
+      <div className={styles.prifileContainer}>
+        <div className={styles.myPrifile}>
+          <img src={ProPic} alt="" />
+          <span className={styles.myPrifileName}>مریم مهاجر</span>
+        </div>
+        <div className={`${styles.inputContainer}`}>
+          <input
+            onChange={changeHandler}
+            type="search"
+            className={styles.searchInCourses}
+            placeholder="جست و جوی دوره"
+          />
+          <span onClick={handleInputClick} className={`${styles.icon}`}>
+            {showIcon && <FaSistrix />}
+          </span>
+        </div>
+      </div>
+      <ul className={`${styles.navigationItems}`}>
+        <li>
+          <span className={styles.navigationItemIcon}>
+            <AiFillHome />
+          </span>
+          <a className={styles.navigationItemLink} href="#">
+            پیشخوان
+          </a>
+        </li>
+
+        {/* <li>
+            <span className={styles.navigationItemIcon}>
+              <MdOutlineModelTraining />
+            </span>
+            <a href="#">آموزش</a>
+          </li> */}
+        <li>
+          <span className={styles.navigationItemIcon}>
+            <FaUserEdit />
+          </span>
+          <a className={styles.navigationItemLink} href="#">
+            ویرایش پروفایل
+          </a>
+        </li>
+        <li>
+          <span className={styles.navigationItemIcon}>
+            <ImList2 />
+          </span>
+          <a className={styles.navigationItemLink} href="#">
+            دوره های من <ImList2 />
+          </a>
+        </li>
+        <li>
+          <span className={styles.navigationItemIcon}>
+            <FaBars />
+          </span>
+          <a className={styles.navigationItemLink} href="#">
+            {" "}
+            لیست دوره ها{" "}
+          </a>
+        </li>
+        <li>
+          <span className={styles.navigationItemIcon}>
+            <IoMdExit />
+          </span>
+          <a className={styles.navigationItemLink} href="#">
+            خروج
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SmallNavSideMenu;
