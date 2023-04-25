942ca7c9044ce035dbaf23dc3cfe9ec7863775db
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-25 23:27:35 +0330

    import bug in Dashboard fixed

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Dashboard from "./components/Dashboard"
-
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
-  return <div className="App">
-
-    <Dashboard/>
-  </div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
