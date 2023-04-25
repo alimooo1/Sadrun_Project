4ac275179a375395160934a0c52039523e025f3a
maryam <maryambabakhan2017@gmail.com>
2023-04-25 17:00:39 +0430

    use Form component in DetailsBill

--- sadrun-project/src/components/Q&A/Form-Q&A/FormQA.jsx
+++ sadrun-project/src/components/Q&A/Form-Q&A/FormQA.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import styles from "./FormQA.module.scss"
+import React from "react";
+import styles from "./FormQA.module.scss";
 
 function FormQA() {
 	return (
 		<div className={styles.formQA}>
-			<input className={styles.inputQA} type="text" placeholder='نام کامل' />
-			<input className={styles.inputQA} type="email" placeholder='ایمیل' />
-			<textarea className={styles.textareaQA} placeholder='متن پیام' />
+			<input className={styles.inputQA} type="text" placeholder="نام کامل" />
+			<input className={styles.QA} type="email" placeholder="ایمیل" />
+			<textarea className={styles.textareaQA} placeholder="متن پیام" />
 		</div>
-	)
+	);
 }
 
 export default FormQA;
