faa5f293d9db79beecf38f9af6b440c787e23f1e
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-26 23:21:22 +0330

    PaymentDetailLayout Developed

--- sadrun-project/src/layouts/PaymentDetail-Layout/PaymentDetailLayout.jsx
+++ sadrun-project/src/layouts/PaymentDetail-Layout/PaymentDetailLayout.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import styles from "./PaymentDetail.module.scss";
+import BuyLevel from "../../components/Buy-Level/BuyLevel";
+import ShoppingCard from "../../components/ShoppingCart/ShoppingCart";
+import CourseInformation from "../../components/CheckoutCard/CheckoutCard";
 
 export default function PaymentDetailLayout() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <BuyLevel level={2} />
+      <div className={styles.bottomPart}>
+        <ShoppingCard />
+        <CourseInformation />
+      </div>
+    </div>
+  );
 }
