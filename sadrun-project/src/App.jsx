cb3d07c5bcb4e8bc44649bb2b70f8d7ad8b7799b
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-26 22:27:50 +0330

    Cleaning App.jsx

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
