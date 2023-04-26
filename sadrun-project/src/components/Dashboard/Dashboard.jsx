e7804153bad99a8995337bf633495c31cf77ce88
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-26 13:57:48 +0330

    Dashboard bugs fixed

--- sadrun-project/src/components/Dashboard/Dashboard.jsx
+++ sadrun-project/src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Dashboard.module.scss";
 import profile from "./img/profile.svg";
 import icon1 from "./img/icon1.svg";
@@ -13,30 +13,46 @@ const dashboardList = [
     title: "پیشخوان",
     icon: icon1,
     to: "/dashboard",
+    picked: true,
   },
   {
     title: "ویرایش پروفایل",
     icon: icon2,
     to: "/dashboard/profile/edit",
+    picked: false,
   },
   {
     title: "دوره های من",
     icon: icon3,
     to: "/dashboard/mycourses",
+    picked: false,
   },
   {
     title: "لیست دوره ها",
     icon: icon4,
     to: "/dashboard/courses",
+    picked: false,
   },
   {
     title: "خروج",
     icon: icon5,
     to: "/logout",
+    picked: false,
   },
 ];
 
 const dashboard = () => {
+  const [menuItems, setMenuItems] = useState(dashboardList);
+
+  const clickHandler = (item) => {
+    dashboardList.forEach((item) => {
+      item.picked = false;
+    });
+    const newList = [...dashboardList];
+    newList[item].picked = true;
+    setMenuItems(newList);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.dashboard}>
@@ -46,8 +62,16 @@ const dashboard = () => {
         </div>
 
         <ul className="down">
-          {dashboardList.map((item, index) => {
-            return <DashboardItem title={item.title} to={item.to} icon={item.icon} />;
+          {menuItems.map((item, index) => {
+            return (
+              <DashboardItem
+                title={item.title}
+                to={item.to}
+                icon={item.icon}
+                picked={item.picked}
+                clickHandler={() => clickHandler(index)}
+              />
+            );
           })}
         </ul>
       </div>
