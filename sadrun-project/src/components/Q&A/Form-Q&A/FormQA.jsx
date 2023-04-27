701d279fb023054c7a12105b7259b138d70b4058
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-27 16:04:39 +0330

    inputQA bug fixed

--- sadrun-project/src/components/Q&A/Form-Q&A/FormQA.jsx
+++ sadrun-project/src/components/Q&A/Form-Q&A/FormQA.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "./FormQA.module.scss";
 
 function FormQA() {
-	return (
-		<div className={styles.formQA}>
-			<input className={styles.inputQA} type="text" placeholder="نام کامل" />
-			<input className={styles.QA} type="email" placeholder="ایمیل" />
-			<textarea className={styles.textareaQA} placeholder="متن پیام" />
-		</div>
-	);
+  return (
+    <div className={styles.formQA}>
+      <input className={styles.inputQA} type="text" placeholder="نام کامل" />
+      <input className={styles.inputQA} type="email" placeholder="ایمیل" />
+      <textarea className={styles.textareaQA} placeholder="متن پیام" />
+    </div>
+  );
 }
 
 export default FormQA;
