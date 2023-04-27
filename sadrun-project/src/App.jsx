96ae76556cd518d786184832e1cb7aa6f1ea937f
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-27 15:22:54 +0330

    Additional Imports removed from App

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import DetailsBill from './components/Details-Bill/DetailsBill';
 
 function App() {
-  return <div className="App">
-<DetailsBill />
-</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
