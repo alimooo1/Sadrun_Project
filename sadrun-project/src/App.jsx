53a711143013b83fd9fe225379e79f363d528096
maryam <maryambabakhan2017@gmail.com>
2023-04-27 13:27:37 +0430

    Detabills done

--- sadrun-project/src/App.jsx
+++ sadrun-project/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import DetailsBill from './components/Details-Bill/DetailsBill';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+<DetailsBill />
+</div>;
 }
 
 export default App;
