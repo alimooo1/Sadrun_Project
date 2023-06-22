4f2246efcd003ede562b8c8076defa797ee81c51
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-06-22 05:24:35 +0330

    Courses and Blogs linked to single course and blog

--- sadrun-project/src/components/Courses/Grid_Courses/Single_Course/SingleCourse.jsx
+++ sadrun-project/src/components/Courses/Grid_Courses/Single_Course/SingleCourse.jsx
@@ -2,26 +2,36 @@ import React from "react";
 import "../../../../sass/fonts.scss";
 import styles from "./style.module.scss";
 import StarRating from "../../../StarRating/StarRating";
-import capacity from './assets/capacity.png';
+import capacity from "./assets/capacity.png";
+import { Link } from "react-router-dom";
 
-export default function SingleCourse({ title, pic, teacherName, price, allCapacity, leftCapacity }) {
+export default function SingleCourse({
+  title,
+  pic,
+  teacherName,
+  price,
+  allCapacity,
+  leftCapacity,
+}) {
   return (
-    <div className={styles.singleCourse}>
-      <img className={styles.poster} src={pic} />
-      <span className={styles.title}>{title}</span>
-      <div className={styles.info}>
-        <div className={styles.right}>
-          <span className={styles.teacher_name}>{teacherName}</span>
-          <StarRating width={11} initialValue={3} voteNum={90313}/>
-          <span className={styles.price}>{price}</span>
-        </div>
+    <Link to="/single-blog" className={styles.singleCourseLink}>
+      <div className={styles.singleCourse}>
+        <img className={styles.poster} src={pic} />
+        <span className={styles.title}>{title}</span>
+        <div className={styles.info}>
+          <div className={styles.right}>
+            <span className={styles.teacher_name}>{teacherName}</span>
+            <StarRating width={11} initialValue={3} voteNum={90313} />
+            <span className={styles.price}>{price}</span>
+          </div>
 
-        <div className={styles.capacity}>
-          <img src={capacity} alt="" />
-          <span className={styles.all_capacity}>{allCapacity}</span>
-          <span className={styles.left_capacity}>{leftCapacity}</span>
+          <div className={styles.capacity}>
+            <img src={capacity} alt="" />
+            <span className={styles.all_capacity}>{allCapacity}</span>
+            <span className={styles.left_capacity}>{leftCapacity}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- sadrun-project/src/components/Courses/List_Courses/Single_Course/SingleCourse.jsx
+++ sadrun-project/src/components/Courses/List_Courses/Single_Course/SingleCourse.jsx
@@ -5,6 +5,7 @@ import StarRating from "../../../StarRating/StarRating";
 import capacity from "./assets/capacity.png";
 import { GiTeacher } from "react-icons/gi";
 import { TbCalendarTime } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 export default function SingleCourse({
   title,
@@ -17,36 +18,38 @@ export default function SingleCourse({
   duration,
 }) {
   return (
-    <div className={styles.singleCourse}>
-      <img className={styles.poster} src={pic} />
+    <Link to="/single-blog" className={styles.singleCourseLink}>
+      <div className={styles.singleCourse}>
+        <img className={styles.poster} src={pic} />
 
-      <div className={styles.info}>
-        <div className={styles.row}>
-          <div className={styles.about}>
-            <span className={styles.title}>{title}</span>
-            <p className={styles.description}>{description}</p>
+        <div className={styles.info}>
+          <div className={styles.row}>
+            <div className={styles.about}>
+              <span className={styles.title}>{title}</span>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.capacity}>
+              <img src={capacity} alt="" />
+              <span className={styles.all_capacity}>{allCapacity}</span>
+              <span className={styles.left_capacity}>{leftCapacity}</span>
+            </div>
           </div>
-          <div className={styles.capacity}>
-            <img src={capacity} alt="" />
-            <span className={styles.all_capacity}>{allCapacity}</span>
-            <span className={styles.left_capacity}>{leftCapacity}</span>
+          <div className={styles.tags}>
+            <div className={styles.icon_box}>
+              <GiTeacher color="#707070" size={17.5} />
+              <span className={styles.icon_title}>مدرس:</span>
+              <span className={styles.teacher_name}>{teacherName}</span>
+            </div>
+            <div className={styles.icon_box}>
+              <TbCalendarTime color="#707070" size={17.5} />
+              <span className={styles.icon_title}>مدت زمان دوره:</span>
+              <span className={styles.duration}>{duration}</span>
+            </div>
+            <StarRating width={13} initialValue={3} voteNum={90313} />
+            <span className={styles.price}>{price}</span>
           </div>
-        </div>
-        <div className={styles.tags}>
-          <div className={styles.icon_box}>
-            <GiTeacher color="#707070" size={17.5}/>
-            <span className={styles.icon_title}>مدرس:</span>
-            <span className={styles.teacher_name}>{teacherName}</span>
-          </div>
-          <div className={styles.icon_box}>
-            <TbCalendarTime color="#707070" size={17.5}/>
-            <span className={styles.icon_title}>مدت زمان دوره:</span>
-            <span className={styles.duration}>{duration}</span>
-          </div>
-          <StarRating width={13} initialValue={3} voteNum={90313} />
-          <span className={styles.price}>{price}</span>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
